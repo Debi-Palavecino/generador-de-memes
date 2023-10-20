0349e43862ea9e9eb87225fa538236b2d3eef617
Deborah.j.Palavecino <77463273+Debi-Palavecino@users.noreply.github.com>
2023-10-19 22:41:03 -0300

    resuelta la mayoria de las variables de la seccion de texto, falta acomodar los diseños de imagen y las funciones para los filtros

--- app.js
+++ app.js
@@ -45,3 +45,99 @@ buttonSectionText.onclick=()=>{
   };
 
   /*En el dia de hoy trabaje a fondo con los modos claro y obscuro proximamente buscare la opcion de modificar el input type range para que quede bien  */
+
+
+  /* variables y funciones para modificar texto  */
+
+  const textUp = document.getElementById("textUP")
+  const textDown = document.getElementById("textDown")
+  const textAreaUP = document.getElementById("textarea-superior")
+ const textAreaDown = document.getElementById("textarea-inferior")
+  const checkboxNoTextUp = document.getElementById("up-no-text")
+  const checkboxNoTextDown = document.getElementById("down-no-text")
+
+ textAreaUP.oninput=()=>{
+    textUp.innerText = textAreaUP.value
+ }
+ textAreaDown.oninput=()=>{
+    textDown.innerText= textAreaDown.value
+ }
+
+ checkboxNoTextUp.onchange=()=>{
+    if(checkboxNoTextUp.checked){
+        textUp.style.display="none"
+    } else{
+        textUp.style.display="block"
+    }
+}
+checkboxNoTextDown.onchange=()=>{
+    if(checkboxNoTextDown.checked){
+        textDown.style.display="none"
+    }else{
+        textDown.style.display="block"
+    }
+}
+/* funciones de tipos de fuentes  */
+
+const fontsType = document.getElementById("fonts")
+const fontsSize = document.getElementById("fonts-sizes")
+fontsType.onchange=()=>{
+    let newFont = fontsType.value
+    textUp.style.fontFamily= newFont
+    textDown.style.fontFamily=newFont
+}
+
+fontsSize.oninput=()=>{
+    let newsize=  fontsSize.value
+    textUp.style.fontSize=newsize+"px"
+    textDown.style.fontSize=newsize+"px"
+}
+
+const buttonTextAlignLeft = document.getElementById("text-align-left")
+const buttonTextAlignCenter = document.getElementById("text-align-center")
+const buttonTextAlignRight = document.getElementById("text-align-right")
+
+
+buttonTextAlignCenter.onclick=()=>{
+    textUp.style.textAlign="center"
+    textDown.style.textAlign="center"
+}
+buttonTextAlignLeft.onclick=()=>{
+    textDown.style.textAlign="left"
+    textUp.style.textAlign="left"
+}
+buttonTextAlignRight.onclick=()=>{
+    textDown.style.textAlign="right"
+    textUp.style.textAlign="right"
+}
+
+/* Funciones de Color de texto y fondo */
+
+const textColor = document.getElementById("color-texto")
+const backColor = document.getElementById("back-color")
+const labelTextColor = document.getElementById("labelText")
+const labelBackColor = document.getElementById("backColor")
+const NoColorBackText =document.getElementById("back-transparent-color")
+
+textColor.oninput=()=>{
+    textUp.style.color=textColor.value
+    textDown.style.color= textColor.value
+    labelTextColor.innerHTML=textColor.value
+}
+
+backColor.oninput=()=>{
+    textUp.style.backgroundColor=backColor.value
+    textDown.style.backgroundColor=backColor.value
+    labelBackColor.innerHTML=backColor.value
+}
+
+NoColorBackText.onchange=()=>{
+    if (NoColorBackText.checked){
+        textUp.style.backgroundColor="transparent"
+        textDown.style.backgroundColor="transparent"
+    }else{
+        textUp.style.backgroundColor=backColor.value
+        textDown.style.backgroundColor=backColor.value
+    }
+}
+
